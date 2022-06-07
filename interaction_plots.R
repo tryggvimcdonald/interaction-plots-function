f789864a1e376953dd9abeaca6ad2606b0d784ea
tryggvimcdonald <107072309+tryggvimcdonald@users.noreply.github.com>
2022-06-07 16:10:47 -0400

    Update interaction_plots.R

--- interaction_plots.R
+++ interaction_plots.R
@@ -1,4 +1,3 @@
-sampledata <- read.csv("C:/Users/pierc/Downloads/SHBG-testdata.csv")
 library(dplyr)
 library(ggplot2)
 library(ggsignif)
@@ -9,58 +8,48 @@ library(plotrix)
 library(ggpubr)
 library(rstatix)
 
-makeplots <- function(plotname,pheno,SNP,exposure,minor_allele,major_allele)
+interaction_plots <- function(tablename, 
+                              pheno, 
+                              SNP, 
+                              exposure, 
+                              nonref_allele, 
+                              ref_allele, 
+                              num_plots = 3, 
+                              export = TRUE)
 {
-  #plotname - name of the plot
+  #tablename - name of the data to be used for the plots
   #pheno - phenotype being measured
   #SNP - SNP name (e.g. rs161896_A)
   #exposure - exposure (e.g. Vegetarianism)
-  #minor_allele - minor allele
-  #major_allele - major allele
-  #a - number of minor alleles (Not yet implemented fully)
+  #nonref_allele - non-reference allele. 0 is interpreted as homozygous non-reference
+  #ref_allele - reference allele. 2 is interpreted as homozygous reference
+  #num_plots - specify number of plot panels to display, can be 1, 2, or 3. Default is 3.
+  #export - FALSE prevents plot output to file
   
-  #Multiple Allele Prompting
-  
-  #if(a == 2)
-  #{
-  # userinput2 <- as_string(readline(prompt = "Minor Allele 2? "))
-  #}
-  
-  #if(a == 3)
-  #{
-  #  userinput2 <- as_string(readline(prompt = "Minor Allele 2? "))
-  #  userinput3 <- as_string(readline(prompt = "Minor Allele 3? "))
-  #}
-  
-  #if(a == 4)
-  #{
-  #  userinput2 <- as_string(readline(prompt = "Minor Allele 2? "))
-  #  userinput3 <- as_string(readline(prompt = "Minor Allele 3? "))
-  #  userinput4 <- as_string(readline(prompt = "Minor Allele 4? "))
-  #}
   
   #Put variables in quotes if they need them to play nicely with other functions
-  plotname <- enquo(plotname)
   pheno <- enquo(pheno)
   SNP <- enquo(SNP)
   exposure <- enquo(exposure)
-  minor_allele <- enquo(minor_allele)
-  major_allele <- enquo(major_allele)
+  nonref_allele <- enquo(nonref_allele)
+  ref_allele <- enquo(ref_allele)
   
+  #Import data from user
+  sampledatachar <- tablename
   
   #Creates a dataframe with mean and sd of SHBG, grouped by allele genotype
-  testdataframe <- summarise( group_by(sampledata, !!SNP),
+  testdataframe <- summarise( group_by(sampledatachar, !!SNP),
                               mean_SHBG = mean(!!pheno),
                               se_SHBG = std.error(!!pheno))
   
   #Split data by exposure and genotype then apply mean and sd functions
-  exposure0 <- summarise( group_by(filter(sampledata, !!exposure == 0), !!SNP),
+  exposure_df0 <- summarise( group_by(filter(sampledatachar, !!exposure == 0), !!SNP),
                           mean_SHBG = mean(!!pheno),
                           se_SHBG = std.error(!!pheno))
-  exposure1 <- summarise( group_by(filter(sampledata, !!exposure == 1), !!SNP),
+  exposure_df1 <- summarise( group_by(filter(sampledatachar, !!exposure == 1), !!SNP),
                           mean_SHBG = mean(!!pheno),
                           se_SHBG = std.error(!!pheno))
-  exposure <- rbind(exposure0, exposure1)
+  exposure_df <- rbind(exposure_df0, exposure_df1)
   
   #First Graph Bound Calculations
   bound_lower <- min(testdataframe$mean_SHBG)-2*max(testdataframe$se_SHBG)
@@ -68,35 +57,29 @@ makeplots <- function(plotname,pheno,SNP,exposure,minor_allele,major_allele)
   bound_upper <- max(testdataframe$mean_SHBG)+3*max(testdataframe$se_SHBG)
   bound_diff <- bound_upper - bound_lower
   
-  signif01 <- max(testdataframe$mean_SHBG) + 
-    max(testdataframe$se_SHBG) + 
-    0.09*bound_diff
+  signif01 <- max(testdataframe$mean_SHBG) + max(testdataframe$se_SHBG) + 0.09*bound_diff
   
-  signif02 <- max(testdataframe$mean_SHBG) + 
-    max(testdataframe$se_SHBG) + 
-    0.18*bound_diff
+  signif02 <- max(testdataframe$mean_SHBG) + max(testdataframe$se_SHBG) + 0.18*bound_diff
   
-  signif03 <- max(testdataframe$mean_SHBG) + 
-    max(testdataframe$se_SHBG) + 
-    0.27*bound_diff
+  signif03 <- max(testdataframe$mean_SHBG) + max(testdataframe$se_SHBG) + 0.27*bound_diff
   
   #SNP Allele Naming
-  homo_minor <- paste(quo_name(minor_allele),
-                      quo_name(minor_allele),
+  homo_nonref <- paste(quo_name(nonref_allele),
+                      quo_name(nonref_allele),
                       sep = "")
   
-  het <- paste(quo_name(minor_allele),
-               quo_name(major_allele),
+  het <- paste(quo_name(ref_allele),
+               quo_name(nonref_allele),
                sep = "")
   
-  homo_major <- paste(quo_name(major_allele),
-                      quo_name(major_allele),
+  homo_ref <- paste(quo_name(ref_allele),
+                      quo_name(ref_allele),
                       sep = "")
   
   #2nd Plot Re-leveling
-  sampledatachar <- mutate(sampledata, Genotype = factor(pull(sampledata, !!SNP),
+  sampledatachar <- mutate(sampledatachar, Genotype = factor(pull(sampledatachar, !!SNP),
                                                          levels = c(0,1,2),
-                                                         labels = c(homo_major, het, homo_minor)))
+                                                         labels = c(homo_nonref, het, homo_ref)))
   
   sampledatachar$Veg2 <- factor(pull(sampledatachar, !!exposure),
                                 levels = c("0", "1"))
@@ -142,36 +125,41 @@ makeplots <- function(plotname,pheno,SNP,exposure,minor_allele,major_allele)
   plotoutput <- plotoutput + stat_pvalue_manual(stat.test3, 
                                                 label = "p.adj.signif", 
                                                 tip.length = 0.0001, 
-                                                size = 6, 
+                                                size = 4, 
                                                 y.position = c(signif01, signif02, signif03))
   plotoutput <- ggpar(plotoutput, legend = "right") + 
     coord_cartesian(ylim = c(bound_lower, bound_upper))
   
   #Set 2nd Plot Bounds
-  bound_lower2 <- min(exposure$mean_SHBG)-2*max(exposure$se_SHBG)
+  bound_lower2 <- min(exposure_df$mean_SHBG)-2*max(exposure_df$se_SHBG)
   bound_lower2[bound_lower2 < 0] <- 0
-  bound_upper2 <- max(exposure$mean_SHBG)+6*max(exposure$se_SHBG)
+  bound_upper2 <- max(exposure_df$mean_SHBG)+6*max(exposure_df$se_SHBG)
   bound_diff2 <- bound_upper2 - bound_lower2
   
   #Pairwise Bracket Locations
-  signif1 <- max(exposure$mean_SHBG[1:3]) + 
-    max(exposure$se_SHBG[1:3]) + 
-    0.09*bound_diff2
-  signif2 <- max(exposure$mean_SHBG[1:3]) + 
-    max(exposure$se_SHBG[1:3]) + 
-    0.18*bound_diff2
-  signif3 <- max(exposure$mean_SHBG[1:3]) +
-    max(exposure$se_SHBG[1:3]) + 
-    0.27*bound_diff2
-  signif4 <- max(exposure$mean_SHBG[4:6]) + 
-    max(exposure$se_SHBG[4:6]) + 
-    0.09*bound_diff2
-  signif5 <- max(exposure$mean_SHBG[4:6]) + 
-    max(exposure$se_SHBG[4:6]) + 
-    0.18*bound_diff2
-  signif6 <- max(exposure$mean_SHBG[4:6]) + 
-    max(exposure$se_SHBG[4:6]) + 
-    0.27*bound_diff2
+  signif1 <- max(exposure_df$mean_SHBG[1:3]) + 
+             max(exposure_df$se_SHBG[1:3]) + 
+             0.09*bound_diff2
+  
+  signif2 <- max(exposure_df$mean_SHBG[1:3]) + 
+             max(exposure_df$se_SHBG[1:3]) + 
+             0.18*bound_diff2
+  
+  signif3 <- max(exposure_df$mean_SHBG[1:3]) + 
+             max(exposure_df$se_SHBG[1:3]) + 
+             0.27*bound_diff2
+  
+  signif4 <- max(exposure_df$mean_SHBG[4:6]) + 
+             max(exposure_df$se_SHBG[4:6]) + 
+             0.09*bound_diff2
+  
+  signif5 <- max(exposure_df$mean_SHBG[4:6]) + 
+             max(exposure_df$se_SHBG[4:6]) + 
+             0.18*bound_diff2
+  
+  signif6 <- max(exposure_df$mean_SHBG[4:6]) + 
+             max(exposure_df$se_SHBG[4:6]) + 
+             0.27*bound_diff2
   
   #Second Plot
   testplot <- ggbarplot(sampledatachar,
@@ -195,29 +183,48 @@ makeplots <- function(plotname,pheno,SNP,exposure,minor_allele,major_allele)
   testplot <- testplot + stat_pvalue_manual(stat.test,
                                             label = "p.adj.signif",
                                             tip.length = bound_diff2/10000,
-                                            size = 6,
+                                            size = 4,
                                             y.position = c(signif1, signif2, signif3, signif4, signif5, signif6)) +
     stat_pvalue_manual(stat.test2,
                        label = "p.adj.signif",
                        tip.length = bound_diff2/10000,
                        y.position = bound_upper2-0.025*bound_diff2,
-                       size = 6)
+                       size = 4)
   
   testplot <- ggpar(testplot, legend = "right") + 
     coord_cartesian(ylim = c(bound_lower2, bound_upper2))
   
-  #Arrange two plots together
-  plot_final <- ggarrange(plotoutput,
-                          testplot,
-                          nrow = 1,
-                          ncol = 2,
-                          common.legend = TRUE,
-                          legend = "bottom")
+  #Arrange plots
+  if(num_plots == 1)
+  {
+    plot_final <- plotoutput
+  }
   
-  plot_final <- annotate_figure(plot_final, top = text_grob(quo_name(plotname)))
+  if(num_plots == 2)
+  {
+    plot_final <- testplot
+  }
+  
+  if(num_plots != 1 && num_plots != 2)
+  {
+    plot_final <- ggarrange(plotoutput,
+                            testplot,
+                            nrow = 1,
+                            ncol = 2,
+                            common.legend = TRUE,
+                            legend = "bottom")
+  }
+  
+  plot_final <- annotate_figure(plot_final,
+                                top = text_grob(c(paste(quo_name(pheno),
+                                                      "by genotype, stratified by",
+                                                      quo_name(exposure)))))
   print(plot_final)
   
   #Export final plot as .png
-  ggexport(plot_final, filename = "C:/Users/pierc/OneDrive/Desktop/SHBG-Plot-Output.png")
+  if(export != FALSE)
+  {
+    ggexport(plot_final, filename = paste(getwd(),"/Interaction-Plot-Output.png", sep = ""))
+  }
   
 }
